3e6c2da56344dca3354a7b2e896192e5ad237852
Mike <yum650350@gmail.com>
2021-12-27 00:22:01 +0800

    feat: update example
    - add some models
    
    - modify layout

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,34 +1,48 @@
 import * as React from 'react';
 
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ScrollView, SafeAreaView } from 'react-native';
 import ThreedViewer from 'react-native-threed-viewer';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ThreedViewer
-        src={{
-          model: require('./assets/ping.obj'),
-          texture: require('./assets/ping.png'),
-        }}
-        style={styles.modalViewer}
-        allowsCameraControl
-      />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <ThreedViewer
+          src={{
+            model: require('./assets/penguin.obj'),
+            texture: require('./assets/penguin.jpg'),
+          }}
+          style={styles.modalViewer}
+          allowsCameraControl
+        />
+        <ThreedViewer
+          src={{
+            model: require('./assets/cat.obj'),
+            texture: require('./assets/cat.jpg'),
+          }}
+          style={styles.modalViewer}
+          allowsCameraControl
+        />
+        <ThreedViewer
+          src={{
+            model: require('./assets/fox.obj'),
+            texture: require('./assets/fox.jpg'),
+          }}
+          style={styles.modalViewer}
+          allowsCameraControl
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'green',
-    flex: 1,
+    marginTop: 20,
     alignItems: 'center',
-    justifyContent: 'center',
   },
   modalViewer: {
-    backgroundColor: 'red',
-    width: 200,
     height: 200,
-    marginVertical: 20,
+    width: 200,
   },
 });
